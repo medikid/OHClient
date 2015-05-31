57c4d39d5f3cac4a4bfbcb09150ec878cc0fa8f2
medikid <soma.programmer@gmail.com>
2015-05-31 08:31:27 -0700

    Fresh Soluton Added

---
+++ OHClient/main.cpp
@@ -0,0 +1,38 @@
+#ifndef _WIN32_WINNT
+# define _WIN32_WINNT 0x602
+#endif
+
+//#include "TCPClient.h"
+//#include  "TCPConn.h"// Must come before boost/serialization headers.
+//#include "Messenger.h" //will load dll, but crash after include MsgQ.h, even though no issues including all includes on it
+
+//#include "Cfg.h" //we get DLL loadng error 126 while loading dll
+#include "OHUser.h"
+
+/////////////////////////////////////////////////////
+
+/////////////////////////////////
+//DLLMAIN
+/////////////////////////////////
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+	//GameState* pGState = new GameState(IOService);
+
+    switch (ul_reason_for_call) {
+                case DLL_PROCESS_ATTACH:
+                        AllocConsole();
+						//MessageBoxA(NULL,"Hello from DLL process atach","Caption",NULL);
+                        break;
+                case DLL_THREAD_ATTACH:
+					//MessageBoxA(NULL,"Hello from DLL thread atach","Caption",NULL);
+                        break;
+                case DLL_THREAD_DETACH:
+					//MessageBoxA(NULL,"Hello from DLL thread detach","Caption",NULL);
+                        break;
+                case DLL_PROCESS_DETACH:
+                        FreeConsole();
+						//MessageBoxA(NULL,"Hello from DLL process detach","Caption",NULL);
+                        break;
+    }
+    return TRUE;
+}
+/////////////////////////////////
